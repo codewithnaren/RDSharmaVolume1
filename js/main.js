6b4df656598ae1aebbb1640b45660e6cff4bd46f
codewithnaren <91406174+codewithnaren@users.noreply.github.com>
2021-10-16 16:40:55 +0530

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-const url = 'https://codewithnaren.github.io/socialandpoliticallife1/Polity6.pdf';
+const url = 'https://codewithnaren.github.io/RDSharmaVolume1/RDSharmaVolume1.pdf';
 
 
 let pdfDoc = null,
